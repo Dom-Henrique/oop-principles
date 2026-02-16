b68f81ac173b96b10e0b6f24a94b23f47b55c443
Dom-Henrique <joaohenriqueds24@gmail.com>
2026-02-16 08:44:45 -0300

    Professor class
    professor name, professor area

---
+++ src/com/seminar/domain/Professor.java
@@ -0,0 +1,27 @@
+package com.seminar.domain;
+
+public class Professor {
+    private String name;
+    private String area;
+
+    public Professor(String name, String area) {
+        this.name = name;
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
