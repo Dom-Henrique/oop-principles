216f2f96b6289392d923f5a30883ed757fd94e17
Dom-Henrique <joaohenriqueds24@gmail.com>
2026-02-25 13:28:47 -0300

    HashMap coursesPayed
    A hashmap to storage payment status by users

--- src/com/course/classes/SysCourse.java
+++ src/com/course/classes/SysCourse.java
@@ -1,12 +1,17 @@
 package com.course.classes;
 
+import com.course.enums.Status;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 // Responsável pelas listas
 public class SysCourse {
     private ArrayList<Users> users = new ArrayList<>();
     private ArrayList<Roll> rolls = new ArrayList<>();
     private ArrayList<Course> courses = new ArrayList<>();
+    private HashMap<Users, Status> coursesPayed = new HashMap<>();
 
     public ArrayList<Users> getUsers() {
         return users;
@@ -30,5 +35,13 @@ public class SysCourse {
 
     public void setCourses(ArrayList<Course> courses) {
         this.courses = courses;
+    }
+
+    public HashMap<Users, Status> getCoursesPayed() {
+        return coursesPayed;
+    }
+
+    public void setCoursesPayed(HashMap<Users, Status> coursesPayed) {
+        this.coursesPayed = coursesPayed;
     }
 }
